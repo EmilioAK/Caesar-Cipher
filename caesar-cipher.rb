4fd7a7040c0498decf2bfb6613a0bdad56d258f1
EmilioAK <emilio.alvarez96@gmail.com>
2020-07-06 16:29:17 +0200

    Re-write cipher to use map instead of each

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,4 +1,4 @@
-def arrayLooper (array, index)
+def modulatedArray (array, index)
     return array[index%array.length()]
 end
 
@@ -12,25 +12,25 @@ end
 
 def shift_char (alphabet, char, number)
     current_pos = alphabet.find_index(char)
-    return arrayLooper(alphabet, current_pos+number)
+    return modulatedArray(alphabet, current_pos+number)
 end
 
 def caesar_cipher (string, number)
     alphabet = ("a".."z").to_a
-    shifted_chars = []
+    string_to_array = string.split("")
     
-    string.split("").each do |char|
+    result = string_to_array.map do |char|
         if char_is_letter(alphabet, char)
             if is_upper?(char)
                 char = char.downcase
                 char = shift_char(alphabet, char, number)
-                shifted_chars.append(char.upcase)
-                next
+                char = char.upcase
             else
                 char = shift_char(alphabet, char, number)
             end
+        else
+            char
         end
-        shifted_chars.append(char)
     end
-    return shifted_chars.join("")
+    return result.join("")
 end
